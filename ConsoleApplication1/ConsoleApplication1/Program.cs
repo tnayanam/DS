746eedfe824aceb079a60aee550b6805ab86b04a
unknown <tnayanam@syr.edu>
2018-04-13 10:31:48 -0400

    Immutable String , Reversal

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
-﻿// Anagram
+﻿// Reverse
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApplication1
 {
@@ -7,51 +8,27 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string str1 = "Listen";
-            string str2 = "Silent";
+            //string str = "Tanuj";
+            //int len = str.Length;
+            // Strings are Immutable so we can only read from string we cannot change it
+            //for (int i = 0; i < len/2; i++)
+            //{
+            //    char temp;
+            //    temp = str[i];
+            //    str[i] = str[len - i - 1];
+            //    str[len - 1 - i] = temp;
+            //}
 
-            //string str1 = "Rooo";
-            //string str2 = "ooR";
+            string str1 = "Hello";
+            string str2 = "";
 
-            //string str1 = "Lisn";
-            //string str2 = "Sile";
-
-
-            bool isAnagram = checkAnagram(str1.ToUpper(), str2.ToUpper());
-
-            System.Console.WriteLine("Palindrome: " + isAnagram);
+            for (int i = str1.Length - 1; i >= 0; i--)
+            {
+                str2 += str1[i];
+            }
+            System.Console.WriteLine(str2);
         }
 
-        public static bool checkAnagram(string str1, string str2)
-        {
-
-            if (str1 == null || str2 == null)
-                return false;
-
-            if (str1.Length != str2.Length)
-                return false;
-
-            int[] arr1 = new int[256];
-            int[] arr2 = new int[256];
-
-            foreach (var c in str1) // at tht ASCII place the value is incremented
-            {
-                arr1[c]++;
-            }
-
-            foreach (var c in str2)
-            {
-                arr2[c]++;
-            }
-
-            for (int i = 0; i < 256; i++)
-            {
-                if (arr1[i] == arr2[i])
-                    continue;
-                else return false;
-            }
-            return true;
-        }
     }
 }
 
