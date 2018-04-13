640674c5f3c09a2a35bc68ad3fa53be94c33890e
unknown <tnayanam@syr.edu>
2018-04-13 09:39:30 -0400

    string palindrome

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Remove duplicate
+﻿// Palindrome
 using System.Collections.Generic;
 
 namespace ConsoleApplication1
@@ -7,50 +7,37 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var str = "Google";
-            RemoveDupe(str);
-            RemoveDupe1(str);
+            string str = "Hellolle";
+            bool isPalindrome = checkPalin(str);
+
+            System.Console.WriteLine("Palindrome: " + isPalindrome);
         }
 
-        public static void RemoveDupe(string str)
+        public static bool checkPalin(string str)
         {
-            Dictionary<char, int> dict = new Dictionary<char, int>();
-
-            foreach (var c in str) // O(N)
+            try
             {
-                if (dict.ContainsKey(c)) // Dictionary cannot have duplicate keys, So we need to put this check and also then we can increment the value instead of adding s duplicate key which will throw the exception.
+                var len = str.Length;
+
+                for (int i = 0; i < len / 2; i++)
                 {
-                    dict[c] = dict[c] + 1;
+                    if (str[i] != str[len - i - 1])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
-                else
-                {
-                    dict.Add(c, 1);
-                }
+                return true;
             }
-            string str1 = "";
-
-            foreach (var f in dict)
+            catch (System.Exception)
             {
-                str1 += f.Key;
-            }
-
-            System.Console.WriteLine(str1);
-        }
-
-
-        public static void RemoveDupe1(string str)
-        {
-            string str1 = "";
-
-            foreach (var f in str)
-            {
-                if (str1.IndexOf(f) == -1)  // index  of character which is not there returns -1
-                {
-                    str1 += f;
-                }
+                // handle execption there for string null type
+                return false;
             }
 
-            System.Console.WriteLine(str1);
         }
     }
 }
@@ -65,49 +52,3 @@ namespace ConsoleApplication1
 
 
 
-
-
-
-
-
-
-// ----------------------------------------------------------------------------------------------------------------------------------------------------//
-
-//using System.Collections.Generic;
-
-//namespace ConsoleApplication1
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            var str = "Enginner ROom";
-//            printDupe(str.ToUpper());
-//        }
-
-//        public static void printDupe(string str)
-//        {
-//            Dictionary<char, int> dict = new Dictionary<char, int>();
-
-//            foreach (var c in str)
-//            {
-//                if (dict.ContainsKey(c)) // checks if key is already present
-//                {
-//                    dict[c] = dict[c] + 1; // if present then add one to the value
-//                }
-//                else
-//                {
-//                    dict.Add(c, 1); // if first time then simply initialize count to 1
-//                }
-//            }
-
-//            foreach (var d in dict)
-//            {
-//                if (d.Value > 1)
-//                {
-//                    System.Console.WriteLine(d.Key);
-//                }
-//            }
-//        }
-//    }
-//}
