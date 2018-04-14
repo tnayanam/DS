3ed4f902f3d80e0ecf6a38d78f70604133858edf
tnayanam <nayatanuj@gmail.com>
2018-04-13 22:52:02 -0400

    Count number of word in a string

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-﻿// Remove duplicate
-
-using System;
-=======
-﻿// Reverse
-using System.Collections.Generic;
->>>>>>> 746eedfe824aceb079a60aee550b6805ab86b04a
-using System.Linq;
+// number of word in a string
 
 namespace ConsoleApplication1
 {
@@ -14,56 +6,25 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            string str1 ="silent";
-            string str2 = "listen";
+            string str = "A new world";
+            bool ot = true;
 
-            //The author didn't mention anything about case sensitivity. If you want to: .Equals(str2, StringComparison.OrdinalIgnoreCase)
-            if (string.Join("", str1.Reverse()) == str2)
+            int count = 0;
+            foreach (var item in str)
             {
-                Console.WriteLine(true);
+                if (item == ' ')
+                {
+                    ot = true;
+                }
+                else if (ot == true)
+                {
+                    ot = false;
+                    count++;
+                }
             }
-            else
-            {
-                Console.WriteLine(false);
-            }
+
+            System.Console.WriteLine(count);
         }
-=======
-            //string str = "Tanuj";
-            //int len = str.Length;
-            // Strings are Immutable so we can only read from string we cannot change it
-            //for (int i = 0; i < len/2; i++)
-            //{
-            //    char temp;
-            //    temp = str[i];
-            //    str[i] = str[len - i - 1];
-            //    str[len - 1 - i] = temp;
-            //}
-
-            string str1 = "Hello";
-            string str2 = "";
-
-            for (int i = str1.Length - 1; i >= 0; i--)
-            {
-                str2 += str1[i];
-            }
-            System.Console.WriteLine(str2);
-        }
-
->>>>>>> 746eedfe824aceb079a60aee550b6805ab86b04a
     }
 }
 
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
->>>>>>> 746eedfe824aceb079a60aee550b6805ab86b04a
