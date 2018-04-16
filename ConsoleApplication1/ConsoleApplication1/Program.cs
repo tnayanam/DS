29cae14837672308064d443ae1c07a1cdac098a1
tnayanam <nayatanuj@gmail.com>
2018-04-16 02:49:35 -0400

    3. Longest Substring HashSet

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,58 +1,42 @@
+// longest substring sequence
+// O(n)
+
+// HashSet is just like List only difference is it does not allow duplicate valuse
+// Add method returns false if duplicate valuess are returned.
+
+using System;
+using System.Collections.Generic;
+
 namespace ConsoleApplication1
 {
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int x) { val = x; }
-    }
-
     class Program
     {
-        public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
-        {
-            int carry = 0;
-            ListNode head = new ListNode(0);
-            ListNode curr = head;
-
-            while (l1 != null || l2 != null)
-            {
-                int x = (l1 != null) ? l1.val : 0;
-                int y = (l2 != null) ? l2.val : 0;
-                int sum = carry + x + y;
-                carry = sum / 10;
-                curr.next = new ListNode(sum % 10);
-                curr = curr.next;
-                if (l1 != null)
-                    l1 = l1.next;
-                if (l2 != null)
-                    l2 = l2.next;
-            }
-            if (carry > 0)
-            {
-                curr.next = new ListNode(carry);
-
-            }
-
-            head = head.next;
-            return head;
-        }
+        //abcade
         static void Main(string[] args)
         {
-            ListNode l1 = new ListNode(2);
-            l1.next = new ListNode(4);
-            l1.next.next = new ListNode(3);
-
-            ListNode l2 = new ListNode(5);
-            l2.next = new ListNode(6);
-            l2.next.next = new ListNode(4);
-
-            ListNode res = addTwoNumbers(l1, l2);
-            while (res != null)
+            Console.WriteLine(LengthOfLongestSubstring("abcba"));
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int i = 0;
+            int j = 0;
+            int maxLength = 0;
+            HashSet<int> h1 = new HashSet<int>();
+            int n = s.Length;
+            while (i < n && j < n)
             {
-                System.Console.Write(res.val + " ");
-                res = res.next;
+                if (h1.Add(s[j]))
+                {
+                    j++;
+                    maxLength = Math.Max(maxLength, j - i);
+                }
+                else
+                {
+                    h1.Remove(s[i]);
+                    i++;
+                }
             }
+            return maxLength;
         }
     }
 }
