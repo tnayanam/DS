cc7354e03bbf6306d2a11443dbae3ed93958ec82
tnayanam <nayatanuj@gmail.com>
2018-04-17 01:00:13 -0400

    longest palindrome

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,62 +1,42 @@
-// O(m+n) 
+// O(n*n) 
 namespace ConsoleApplication1
 {
     class Program
     {
-        public static void findMedian(int[] arr3)
-        {
-            int mid = arr3.Length / 2;
-            double median;
-            System.Console.WriteLine(arr3[mid]);
-            System.Console.WriteLine(arr3[mid - 1]);
-            System.Console.WriteLine((arr3[mid] + arr3[mid - 1]) / 2);
-            if (mid % 2 == 0)
-            {
-                median = (double)(arr3[mid] + arr3[mid - 1]) / 2;
-            }
-            else
-            {
-                median = arr3[mid];
-            }
-            System.Console.WriteLine("the median is: " + median);
-        }
-
         static void Main(string[] args)
         {
-            int[] arr1 = new int[] { 1, 3 };
-            int[] arr2 = new int[] { 2, 4 };
-            int[] arr3 = new int[arr1.Length + arr2.Length];
-            int i = 0, j = 0, k = 0;
-            while (i < arr1.Length && j < arr2.Length)
+            string str = "abqbba";
+            System.Console.WriteLine(LongestPalindraomeString(str));
+        }
+
+        public static string LongestPalindraomeString(string str)
+        {
+            var longest = str.Substring(0, 1);
+            for (int i = 0; i < str.Length - 1; i++)
             {
-                if (arr1[i] <= arr2[j])
+                var s1 = ExpandAroundCenter(str, i, i);
+                if (s1.Length > longest.Length)
                 {
-                    arr3[k] = arr1[i];
-                    i++;
+                    longest = s1;
                 }
-                else
+                var s2 = ExpandAroundCenter(str, i, i + 1); // string like "abba" their center is in middle of two b's
+                if (s2.Length > longest.Length)
                 {
-                    arr3[k] = arr2[j];
-                    j++;
+                    longest = s2;
                 }
-                k++;
             }
+            return longest;
+        }
 
-            while (i < arr1.Length)
+        public static string ExpandAroundCenter(string str, int left, int right)
+        {
+            while (left >= 0 && right <= str.Length && str[left] == str[right])
             {
-                arr3[k] = arr1[i];
-                i++;
-                k++;
+                left--;
+                right++;
             }
-
-            while (j < arr2.Length)
-            {
-                arr3[k] = arr2[j];
-                j++;
-                k++;
-            }
-
-            findMedian(arr3);
+            return str.Substring(left + 1, right - left - 1);  // since after this loop left has decremeted one extra nd right has incremented on extra.
         }
     }
+
 }
