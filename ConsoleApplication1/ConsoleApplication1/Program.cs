c68452925513322dca5643fc1ee2258adc300033
unknown <tnayanam@syr.edu>
2018-04-18 10:59:46 -0400

    12. roman to integer

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,22 +8,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsPalindrome(5225));
-        }
-
-        public static bool IsPalindrome(int x)
-        {
-            if (x < 0)
-                return false;
-            int rev = 0;
-            int orig = x;
-
-            while (x != 0)
-            {
-                rev = rev * 10 + x % 10;
-                x = x / 10;
-            }
-            return (rev == orig) ? true : false;
+            int num = 549;
+            string[] str1 = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] str2 = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "Xc" };
+            string[] str3 = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string res = str3[(num % 1000) / 100] + str2[(num % 100) / 10] + str1[num % 10];
 
         }
     }
