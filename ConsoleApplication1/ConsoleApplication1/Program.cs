f0de160750b3ff97da7f392f8407db9a482846bc
tnayanam <nayatanuj@gmail.com>
2018-04-21 00:09:43 -0400

    19. Nth node from last

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,40 +1,71 @@
+// Remove nth node from last
 
-//4 SUM O(N*N) Tuple and dictionary
 using System;
-using System.Collections.Generic;
 namespace ConsoleApplication1
 {
-    class Program
+    public class Node
     {
+        public int data;
+        public Node Next;
+        public Node(int value)
+        {
+            data = value;
+            Next = null;
+        }
+    }
+
+    public class Program
+    {
+        public static Node RemoveNodeFromList(Node head, int fromLast)
+        {
+            Node curr = head;
+            int lenOfLL = 0;
+            while (curr != null)
+            {
+                lenOfLL++;
+                curr = curr.Next;
+            }
+            Node temp = head;
+            Node old = head;
+            if (lenOfLL < fromLast || fromLast < 1)
+                return null;
+            else if (lenOfLL == fromLast)
+            {
+                head = head.Next;
+            }
+            else
+            {
+                for (int i = 0; i < lenOfLL - fromLast; i++)
+                {
+                    old = temp;
+                    temp = temp.Next;
+                }
+                old.Next = temp.Next;
+            }
+            return head;
+        }
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 1,2 , 10, 20, 30, 40};
-            Dictionary<int, Tuple<int, int>> dict = new Dictionary<int, Tuple<int, int>>();
-            int len = arr.Length;
-            int sum = 91;
-            for (int i = 0; i < len-1; i++)
+            Node n1 = new Node(1);
+            n1.Next = new Node(2);
+            n1.Next.Next = new Node(3);
+            n1.Next.Next.Next = new Node(4);
+            n1.Next.Next.Next.Next = new Node(5);
+            n1.Next.Next.Next.Next.Next = new Node(6);
+            Node curr = n1;
+            while (curr != null)
             {
-                for (int j = i+1; j < len; j++)
-                {
-                    if (!dict.ContainsKey(arr[i] + arr[j]))
-                    dict.Add((arr[i] + arr[j]), new Tuple<int, int>(i, j));
-                }
+                Console.Write(curr.data + " ");
+                curr = curr.Next;
             }
-            for(int i =0;i<dict.Count ; i++)
+            Node head = RemoveNodeFromList(n1, 3);
+            Console.WriteLine(" ");
+            curr = head;
+
+            while (curr != null)
             {
-                  for (int j = i+1; j < len; j++)
-                  {
-                      int tempsum = arr[i] + arr[j];
-                      if(dict.ContainsKey(sum - tempsum))
-                      {
-                          Tuple<int, int> t1 = new Tuple<int, int>(dict[sum - tempsum].Item1,dict[sum - tempsum].Item2);
-                          Console.WriteLine(t1.Item1);
-                          Console.WriteLine(t1.Item2);
-                          Console.WriteLine(j);
-                          Console.WriteLine(i);
-                          return;
-                      }
-                  }
+                Console.Write(curr.data + " ");
+                curr = curr.Next;
             }
         }
     }
