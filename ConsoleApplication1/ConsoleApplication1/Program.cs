32bee03d5841e3c8ae099b59bd1d80d55a88da6b
tnayanam <nayatanuj@gmail.com>
2018-04-21 02:59:15 -0400

    21 Merge two sorted LL

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,59 +1,68 @@
-using System.Collections.Generic;
-
 namespace ConsoleApplication1
 {
-    public class Program
+    public class Node
     {
-        public static bool Check(string str)
+        public int data;
+        public Node Next;
+        public Node(int value)
         {
-            Stack<char> st = new Stack<char>();
-            foreach (var item in str)
+            data = value;
+            Next = null;
+        }
+    }
+    public class Main1
+    {
+        public static Node Sort(Node head1, Node head2)
+        {
+            Node sortedHead = new Node(0);
+            Node n3 = sortedHead;
+            Node start1 = head1;
+            Node start2 = head2;
+            while (start1 != null && start2 != null)
             {
-                if (item == '(' || item == '{' || item == '[')
+                if (start1.data > start2.data)
                 {
-                    st.Push(item);
-                }
-                else if(st.Count == 0)
-                {
-                    return false;
+                    n3.Next = new Node(start2.data);
+                    start2 = start2.Next;
                 }
                 else
                 {
-                    if (item == ')')
-                    {
-                        if (st.Peek() == '(')
-                        {
-                            st.Pop();
-                        }
-                        else
-                            return false;
-                    }
-                    if (item == '}')
-                    {
-                        if (st.Peek() == '{')
-                        {
-                            st.Pop();
-                        }
-                        else
-                            return false;
-                    }
-                    if (item == ']')
-                    {
-                        if (st.Peek() == '[')
-                        {
-                            st.Pop();
-                        }
-                        else
-                            return false;
-                    }
+                    n3.Next = new Node(start1.data);
+                    start1 = start1.Next;
                 }
+                n3 = n3.Next;
             }
-            return true;
+
+            while (start1 != null)
+            {
+                n3.Next = new Node(start1.data);
+                n3 = n3.Next;
+                start1 = start1.Next;
+            }
+
+            while (start2 != null)
+            {
+                n3.Next = new Node(start2.data);
+                n3 = n3.Next;
+                start2 = start2.Next;
+            }
+            return sortedHead.Next;
         }
+
         static void Main(string[] args)
         {
-            string str = "((}))";
-            System.Console.WriteLine(Check(str));
+            Node head1 = new Node(1);
+            head1.Next = new Node(5);
+            head1.Next.Next = new Node(6);
+            Node head2 = new Node(0);
+            head2.Next = new Node(7);
+            //head2.Next.Next = new Node(8);
+            Node start = Sort(head1, head2);
+            while (start != null)
+            {
+                System.Console.Write(start.data + " ");
+                start = start.Next;
+            }
         }
     }
 }
