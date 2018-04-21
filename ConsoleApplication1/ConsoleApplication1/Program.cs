2857efdaa24a6d35a320514d7b70b24653b60420
tnayanam <nayatanuj@gmail.com>
2018-04-21 12:56:15 -0400

    Shuffle deck of cards

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,43 +1,27 @@
-using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApplication1
 {
     public class Node
     {
-
-        public static IList<string> GenerateParenthesis(int n)
-        {
-            List<string> result = new List<string>();
-            if (n <= 0) return result;
-            GenerateParenthesis(n, n, "", result);
-            return result;
-        }
-        private static void GenerateParenthesis(int left, int right, string item, List<string> result)
-        {
-            if (right < left) return;
-            if (left == 0 && right == 0)
-            {
-                result.Add(item);
-            }
-            if (left > 0)
-            {
-                GenerateParenthesis(left - 1, right, item + "(", result);
-            }
-            if (right > 0)
-            {
-                GenerateParenthesis(left, right - 1, item + ")", result);
-            }
-
-        }
         static void Main(string[] args)
         {
-            var list = GenerateParenthesis(3);
+            System.Random rnd = new System.Random();
 
-            foreach (var item in list)
+            var numbers = Enumerable.Range(1, 52).OrderBy(r => rnd.Next(1, 52)).ToArray();
+
+            int[] numb = Enumerable.Range(1, 52).ToArray();
+
+            foreach (var item in numbers)
             {
-                System.Console.WriteLine(item);
+                System.Console.Write(item + " ");
             }
 
+
+            foreach (var item in numb)
+            {
+                System.Console.Write(item + " ");
+            }
         }
     }
 }
