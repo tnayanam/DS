57f5f740b6547adfef37a90f3cfd0f9af6abcae3
tnayanam <nayatanuj@gmail.com>
2018-04-21 00:51:01 -0400

    20. Valid Paranthesis

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,73 +1,59 @@
-// Remove nth node from last
+using System.Collections.Generic;
 
-using System;
 namespace ConsoleApplication1
 {
-    public class Node
-    {
-        public int data;
-        public Node Next;
-        public Node(int value)
-        {
-            data = value;
-            Next = null;
-        }
-    }
-
     public class Program
     {
-        public static Node RemoveNodeFromList(Node head, int fromLast)
+        public static bool Check(string str)
         {
-            Node start = new Node(0);
-            start.Next = head;
-            Node fast = head;
-            Node slow = head;
-            for (int i = 0; i < fromLast; i++)
+            Stack<char> st = new Stack<char>();
+            foreach (var item in str)
             {
-                fast = fast.Next;
-            }
-            if (fast == null)
-            {
-                head = head.Next;
-            }
-            else
-            {
-                while (fast.Next != null)
+                if (item == '(' || item == '{' || item == '[')
                 {
-                    slow = slow.Next;
-                    fast = fast.Next;
+                    st.Push(item);
                 }
-                slow.Next = slow.Next.Next;
+                else if(st.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (item == ')')
+                    {
+                        if (st.Peek() == '(')
+                        {
+                            st.Pop();
+                        }
+                        else
+                            return false;
+                    }
+                    if (item == '}')
+                    {
+                        if (st.Peek() == '{')
+                        {
+                            st.Pop();
+                        }
+                        else
+                            return false;
+                    }
+                    if (item == ']')
+                    {
+                        if (st.Peek() == '[')
+                        {
+                            st.Pop();
+                        }
+                        else
+                            return false;
+                    }
+                }
             }
-
-
-            return head;
+            return true;
         }
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            n1.Next = new Node(2);
-            n1.Next.Next = new Node(3);
-            n1.Next.Next.Next = new Node(4);
-            n1.Next.Next.Next.Next = new Node(5);
-            n1.Next.Next.Next.Next.Next = new Node(6);
-            n1.Next.Next.Next.Next.Next.Next = new Node(7);
-            n1.Next.Next.Next.Next.Next.Next.Next = new Node(8);
-            Node curr = n1;
-            while (curr != null)
-            {
-                Console.Write(curr.data + " ");
-                curr = curr.Next;
-            }
-            Node head = RemoveNodeFromList(n1, 8);
-            Console.WriteLine(" ");
-            curr = head;
-
-            while (curr != null)
-            {
-                Console.Write(curr.data + " ");
-                curr = curr.Next;
-            }
+            string str = "((}))";
+            System.Console.WriteLine(Check(str));
         }
     }
 }
