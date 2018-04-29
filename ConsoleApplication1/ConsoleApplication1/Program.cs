9e54172ac9f17570688feacd393b69071ac3bb62
tnayanam <nayatanuj@gmail.com>
2018-04-28 22:21:35 -0400

    38, Count and Say

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,36 +1,40 @@
-using System;
-
-// To execute C#, please define "static void Main" on a class
-// named Solution.
-// Insert Position
-
-class Solution
+namespace MainSolution
 {
-
-    public static int FindIndex(int[] arr, int left, int right, int elem)
+    class Program
     {
+        public static void Print(int n)
+        {
+            string str = "1";
+            for (int i = 1; i < n; i++)
+            {
+                str = CountAndSay(str);
+            }
+        }
 
-        if (left > right)
-            return left;
-
-        int mid = (left + right) / 2;
-
-        if (arr[mid] == elem)
-            return left;
-
-        if (arr[mid] > elem)
-            return FindIndex(arr, left, mid - 1, elem);
-        else
-            return FindIndex(arr, mid + 1, right, elem);
-    }
-
-    static void Main(string[] args)
-    {
-        int[] arr = new int[] { 1, 3, 5, 6 };
-        int elem = 2;
-
-        Console.Write(FindIndex(arr, 0, arr.Length - 1, elem));
+        public static string CountAndSay(string s)
+        {
+            string ans = "";
+            int cnt = 1;
+            int len = s.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (i + 1 < len && s[i] != s[i + 1])
+                {
+                    ans = ans + cnt.ToString() + s[i];
+                    cnt = 1;
+                }
+                else if (i + 1 < len)
+                {
+                    cnt++;
+                }
+            }
+            ans = ans + cnt.ToString() + s[len - 1]; // for the last similar elements we need this. do a dry run on 1211 and you will understand becasue at the end
+            // we will only have the count we need to append to the element too that is why below line. 
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            Print(36);
+        }
     }
 }
-
-
