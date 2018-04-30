c469c259d7633c664e055272dd24ce73fe8276cc
tnayanam <nayatanuj@gmail.com>
2018-04-30 01:47:56 -0400

    50. Calculate Power

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,37 +1,26 @@
-// Jump 2
-//For example:
-//* Given array A = [2, 3, 1, 1, 4]
-using System;
-
 namespace MainSolution
 {
     class Program
     {
-        public static int findMinJump(int[] arr)
+        public static float CalcPow(int x, int y)
         {
-            if (arr == null || arr.Length == 0)
-                return 0;
-            int lastReach = 0;
-            int reach = 0;
-            int steps = 0;
-            for (int i = 0; i <= reach && i < arr.Length; i++)
+            if (y == 0)
+                return 1;
+            float temp;
+            temp = CalcPow(x, y / 2);
+            if (y % 2 == 0)
+                return temp * temp;
+            else
             {
-                if (i > lastReach)
-                {
-                    steps++;
-                    lastReach = reach;
-                }
-                reach = Math.Max(reach, arr[i] + 1);
+                if (y > 0)
+                    return x * temp * temp;
+                else
+                    return (temp * temp) / 2;
             }
-
-            if (reach < arr.Length - 1) return 0;
-            return steps;
         }
         public static void Main()
         {
-            int[] arr = new int[] { 2, 3, 1, 1, 4 };
-
-            Console.WriteLine(findMinJump(arr));
+            System.Console.WriteLine(CalcPow(2, 6)); // 16
         }
     }
 }
