9d114e327eb70c87cef425ee4cbc2144a1638269
tnayanam <nayatanuj@gmail.com>
2018-05-16 00:42:55 -0400

    Matrix multiplication Wayfair

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,65 +1,53 @@
 using System;
-using System.Collections.Generic;
-namespace ConsoleApplication1
-{
-    public delegate int AddDelagate(int num1, int num2);
 
-    class Test
-    {
-        public int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
-    }
+namespace Multiplication
+{
     class Program
     {
         static void Main(string[] args)
         {
-            string str = "1d6 + 2d20 - 25";
-            string[] token = str.Split(' ');
-            int length = token.Length;
-            int total = ProcessString(token[token.Length - 1]);
-            Stack<string> s1 = new Stack<string>();
-            for (int i = 0; i < token.Length; i++)
+            int start = 9;
+            int end = 12;
+            int index = end - start + 2;
+            int firstRow = 9;
+            int firstColumn = 9;
+
+            int[,] matrix = new int[index, index];
+
+            for (int i = 0; i < index; i++)
             {
-                if (i % 2 != 0)
+                for (int j = 0; j < index; j++)
                 {
-                    s1.Push(token[i]);
+                    if (i == 0 && j == 0)
+                    {
+                        matrix[i, j] = 0;
+                        continue;
+                    }
+                    if (i == 0)
+                    {
+                        matrix[i, j] = firstRow;
+                        firstRow++;
+                    }
+                    else if (j == 0)
+                    {
+                        matrix[i, j] = firstColumn;
+                        firstColumn++;
+                    }
+                    else
+                    {
+                        matrix[i, j] = matrix[i, 0] * matrix[0, j];
+                    }
                 }
             }
-            for (int i = token.Length - 3; i >= 0; )
+            //output
+            for (int i = 0; i < index; i++)
             {
-                var val = ProcessString(token[i]);
-                string oper = s1.Pop();
-                switch (oper)
+                Console.WriteLine();
+                for (int j = 0; j < index; j++)
                 {
-                    case "+":
-                        total = total + val;
-
-                        break;
-                    case "-":
-                        total = val - total;
-                        break;
+                    Console.Write(matrix[i, j] + " ");
                 }
-                i = i - 2;
             }
-            Console.WriteLine("total: " + total);
-        }
-
-        public static int ProcessString(string str)
-        {
-            string[] tok = str.Split('d');
-            if (tok.Length == 1)
-                return Convert.ToInt32(tok[0]);
-
-            int intial = Convert.ToInt32(tok[0]);
-            int end = Convert.ToInt32(tok[1]);
-            Random r1 = new Random();
-            int randomValue = r1.Next(intial, end);
-            Console.WriteLine(randomValue);
-            return randomValue;
         }
     }
 }
-
-
