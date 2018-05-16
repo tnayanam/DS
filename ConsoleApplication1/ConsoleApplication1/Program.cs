692c1379b1089518b2c82c8776d9ba46c94ed0cf
tnayanam <nayatanuj@gmail.com>
2018-05-16 02:38:45 -0400

    Zig Zag Order in matrix

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,19 +1,10 @@
-// Rotate a Matrix ClockWise
+// print matrix zig zag order
 /*
  * 1 2 3
  * 4 5 6
  * 7 8 9
- * 
- * 1 4 7
- * 2 5 8
- * 3 6 9
- * 
- * 7 4 1
- * 8 5 2
- * 9 6 3
  */
 
-using System;
 namespace Multiplication
 {
     class Program
@@ -22,69 +13,42 @@ namespace Multiplication
         {
             int[,] matrix = new int[,]
             {
-                { 1,2,3 },
-                { 4,5,6 },
-                { 7,8,9 }
+                { 1,2,3,4 },
+                { 5,6,7,8 },
+                { 9,10,11,12 }
             };
 
-            for (int i = 0; i < 3; i++)
+            System.Console.WriteLine(matrix.GetUpperBound(0));//2
+            System.Console.WriteLine(matrix.GetUpperBound(1));//4
+
+            for (int i = 0; i <= matrix.GetUpperBound(0); i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                    Console.Write(matrix[i, j] + " ");
+                    System.Console.Write(matrix[i, j] + " ");
                 }
-                Console.WriteLine();
+                System.Console.WriteLine();
             }
 
-            Transpose(matrix);
-            // transposed matrix
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i <= matrix.GetUpperBound(0); i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                    Console.Write(matrix[i, j] + " ");
+                    if (i % 2 != 0)
+                    {
+                        System.Console.Write(matrix[i, matrix.GetUpperBound(1) - j] + " ");
+                    }
+                    else
+                    {
+                        System.Console.Write(matrix[i, j] + " ");
+                    }
+
                 }
-                Console.WriteLine();
+                //System.Console.WriteLine();
             }
 
-            ReverseColumn(matrix);
 
-            // rotate matrix
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
 
-        public static void ReverseColumn(int[,] matrix)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0, k = 2; j < k; k--, j++)
-                {
-                    var temp = matrix[i, j];
-                    matrix[i, j] = matrix[i, k];
-                    matrix[i, k] = temp;
-                }
-            }
-        }
-
-        public static void Transpose(int[,] matrix)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = i; j < 3; j++)
-                {
-                    var temp = matrix[i, j];
-                    matrix[i, j] = matrix[j, i];
-                    matrix[j, i] = temp;
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
