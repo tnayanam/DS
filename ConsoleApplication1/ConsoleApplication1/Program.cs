93819fd4c006b976c10b1ac7b7e8b4d99b31f369
tnayanam <nayatanuj@gmail.com>
2018-05-26 20:43:25 -0400

    KMP Pattern Searching

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,25 +1,79 @@
-// Sub String Occurence Count
+using System.Collections.Generic;
 
-using System;
-
-namespace ConsoleApplication1
+public class Program
 {
-    class Program
+
+    static void Main(string[] args)
     {
-        //abcade
-        static void Main(string[] args)
+        foreach (var item in SearchString("abbcddddcccaaaddcc", "ddcc"))
         {
-            string longstr = "tanuj is a good is id sis";
-            string smallstr = "is";
-            int i = 0;
-            int j = 0;
-            int count = 0;
-            while ((i = longstr.IndexOf(smallstr,i))!=-1)
+            System.Console.Write(item + " ");
+        }
+        //System.Console.WriteLine(SearchString("tasnuj", "as"));
+    }
+
+    public static int[] SearchString(string str, string pat)
+    {
+        List<int> retVal = new List<int>();
+        int M = pat.Length;
+        int N = str.Length;
+        int i = 0;
+        int j = 0;
+        int[] lps = new int[M];
+
+        ComputeLPSArray(pat, lps);
+
+        while (i < N)
+        {
+            if (pat[j] == str[i])
             {
-                i = i + smallstr.Length;
-                count++;
+                j++;
+                i++;
             }
-            Console.WriteLine(count);
+
+            if (j == M)
+            {
+                retVal.Add(i - j);
+                j = lps[j - 1];
+            }
+
+            else if (i < N && pat[j] != str[i])
+            {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+
+        return retVal.ToArray();
+    }
+
+    public static void ComputeLPSArray(string pat, int[] arr)
+    {
+        int len = arr.Length;
+        int i = 0;
+        int j = 1;
+        while (j < len)
+        {
+            if (pat[i] == pat[j])
+            {
+                i++; // length of previous longest prefix
+                arr[j] = i;
+                j++;
+            }
+            else
+            {
+                if (i != 0)
+                {
+                    i = arr[j - 1];
+
+                }
+                {
+                    arr[j] = 0;
+                    j++;
+                }
+            }
         }
     }
 }
