6278de02b67bc2a87bc5fa5e853761f9b6215c2f
tnayanam <nayatanuj@gmail.com>
2018-05-28 16:52:12 -0400

    75. Sort Colours

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace MainSolution
             int num = 24;
             for (int i = 1; i <= num / 2; i++)
             {
-                if ((i * i) <= num && (i+1) * (i+1) > num)
+                if ((i * i) <= num && (i + 1) * (i + 1) > num)
                 {
                     Console.Write(i);
                     break;
@@ -18,3 +18,4 @@ namespace MainSolution
         }
     }
 }
+// 75 is same as Dutch Flag segregate 0,1,2
