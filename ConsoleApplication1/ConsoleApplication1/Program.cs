5cbfb60d1bca903cc94c3ee80345d9abd808afc2
tnayanam <nayatanuj@gmail.com>
2018-05-28 16:33:36 -0400

    69. Square Root

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,78 +1,19 @@
+using System;
+
 namespace MainSolution
 {
     class Program
     {
         public static void Main()
         {
-            // matrix
-            int[,] matrix = new int[,]
+            int num = 24;
+            for (int i = 1; i <= num / 2; i++)
             {
-    {1,0,1,1},
-    {1,1,1,0},
-    {0,1,1,1},
-    {1,1,1,1},
-    {1,1,1,1}
-            };
-            bool firstColumn = false;
-            bool firstRow = false;
-            int rowLength = matrix.GetLength(0);
-            int columnLength = matrix.GetLength(1);
-            //check first column 0
-            for (int i = 0; i < rowLength; i++)
-            {
-                if (matrix[i, 0] == 0)
-                    firstColumn = true;
-            }
-            //check first row 0
-            for (int i = 0; i < columnLength; i++)
-            {
-                if (matrix[0, i] == 0)
-                    firstRow = true;
-            }
-            // mark first row and first column as ZERO
-            for (int i = 1; i < rowLength; i++)
-            {
-                for (int j = 1; j < columnLength; j++)
+                if ((i * i) <= num && (i+1) * (i+1) > num)
                 {
-                    if (matrix[i, j] == 0)
-                    {
-                        matrix[i, 0] = 0;
-                        matrix[0, j] = 0;
-                    }
+                    Console.Write(i);
+                    break;
                 }
-            }
-            // make all rows and columns zero based on mark
-            for (int i = 1; i < rowLength; i++)
-            {
-                for (int j = 1; j < columnLength; j++)
-                {
-                    if (matrix[i, 0] == 0 || matrix[0, j] == 0)
-                    {
-                        matrix[i, j] = 0;
-                    }
-                }
-            }
-            // make firstrow and firstcolumn as 0 based	 on bool
-            for (int i = 0; i < columnLength; i++)
-            {
-                if (firstColumn)
-                    matrix[0, i] = 0;
-            }
-
-            // make firstcolumn and firstcolumn as 0 based	 on bool
-            for (int i = 0; i < rowLength; i++)
-            {
-                if (firstRow)
-                    matrix[i, 0] = 0;
-            }
-
-            for (int i = 0; i < rowLength; i++)
-            {
-                for(int j = 0;j<columnLength;j++)
-                {
-                    System.Console.Write(matrix[i,j] + "    ");
-                }
-                System.Console.WriteLine();
             }
         }
     }
