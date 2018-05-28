7b65c25b59a920acf7854e244061c2265c698516
tnayanam <nayatanuj@gmail.com>
2018-05-28 02:05:54 -0400

    66. Plus One

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -2,16 +2,34 @@ using System;
 
 namespace MainSolution
 {
-	class Program
-	{
+    class Program
+    {
         public static void Main()
-		{
-            string str = "tanuj is a good boys    ";
-            bool spaceFound = false;
-            string[] strArr = str.Trim().Split(' ');
-            Console.WriteLine(strArr[strArr.Length - 1].Length);
+        {
 
+            int[] arr = new int[] { 9, 9, 9 };
+            var newArr = PlusOne(arr);
 
+            foreach (var item in newArr)
+            {
+                Console.Write(item);
+            }
+        }
+        public static int[] PlusOne(int[] digits)
+        {
+            int carry = 1;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                int digit = (digits[i] + carry) % 10;
+                 carry = (digits[i] + carry) / 10;
+                digits[i] = digit;
+                if (carry == 0)
+                    return digits;
+            }
+
+            int[] res = new int[digits.Length + 1];
+            res[0] = 1;
+            return res;
+        }
         }
     }
-}
