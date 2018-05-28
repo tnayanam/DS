5e0084164d8df3a7c2b13c8d8e4854c825362baa
tnayanam <nayatanuj@gmail.com>
2018-05-28 16:00:27 -0400

    73 Matrix Set Zeroes

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,24 +4,76 @@ namespace MainSolution
     {
         public static void Main()
         {
-            int[,] matrix = new int[,]{
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
+            // matrix
+            int[,] matrix = new int[,]
+            {
+    {1,0,1,1},
+    {1,1,1,0},
+    {0,1,1,1},
+    {1,1,1,1},
+    {1,1,1,1}
             };
-            int rows = matrix.GetLength(0);
-            int columns = matrix.GetLength(1);
-            System.Console.WriteLine(NoOfPaths(rows, columns));
-        }
+            bool firstColumn = false;
+            bool firstRow = false;
+            int rowLength = matrix.GetLength(0);
+            int columnLength = matrix.GetLength(1);
+            //check first column 0
+            for (int i = 0; i < rowLength; i++)
+            {
+                if (matrix[i, 0] == 0)
+                    firstColumn = true;
+            }
+            //check first row 0
+            for (int i = 0; i < columnLength; i++)
+            {
+                if (matrix[0, i] == 0)
+                    firstRow = true;
+            }
+            // mark first row and first column as ZERO
+            for (int i = 1; i < rowLength; i++)
+            {
+                for (int j = 1; j < columnLength; j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        matrix[i, 0] = 0;
+                        matrix[0, j] = 0;
+                    }
+                }
+            }
+            // make all rows and columns zero based on mark
+            for (int i = 1; i < rowLength; i++)
+            {
+                for (int j = 1; j < columnLength; j++)
+                {
+                    if (matrix[i, 0] == 0 || matrix[0, j] == 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            // make firstrow and firstcolumn as 0 based	 on bool
+            for (int i = 0; i < columnLength; i++)
+            {
+                if (firstColumn)
+                    matrix[0, i] = 0;
+            }
 
-        public static int NoOfPaths(int rows, int columns)
-        {
-            if (rows == 1 || columns == 1)
-                return 1;
-            else
-                return NoOfPaths(rows, columns - 1) + NoOfPaths(rows - 1, columns) + NoOfPaths(rows - 1, columns - 1);
+            // make firstcolumn and firstcolumn as 0 based	 on bool
+            for (int i = 0; i < rowLength; i++)
+            {
+                if (firstRow)
+                    matrix[i, 0] = 0;
+            }
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for(int j = 0;j<columnLength;j++)
+                {
+                    System.Console.Write(matrix[i,j] + "    ");
+                }
+                System.Console.WriteLine();
+            }
         }
     }
 }
-
-// movement can only be right and down and Diagonally
