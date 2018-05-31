f68141a85424b967fb08af26191bff305b934917
tnayanam <nayatanuj@gmail.com>
2018-05-30 22:58:27 -0400

    74. Search 2D Matrix

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,58 +4,33 @@ namespace MainSolution
     {
         public static void Main()
         {
-            char[,] matrix = new char[,]
-            {
-                {'G','E','E','K','S', 'F', 'O', 'R', 'G', 'E', 'E', 'K', 'S'},
-                { 'G','E','E','K','S','Q','U','I','Z','G','E','E','K'},
-                {'I','D','E','Q','A','P','R','A','C','T','I','C','E'}
-            };
-            PatternSearch(matrix, "EEE");
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    System.Console.Write(matrix[i, j] + " ");
-                }
-                System.Console.WriteLine();
-            }
+            int[,] matrix = new int[,]
+                             {
+                                {1,  2,  3},
+                                {4,  5,  6},
+                                {7,  8,  9},
+                                {10, 11, 12},
+                                {13, 14, 15}
+                             };
+            System.Console.WriteLine(Search(matrix,1));
         }
-        public static void PatternSearch(char[,] matrix, string word)
+
+        public static bool Search(int[,] matrix, int num)
         {
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            int rowCount = matrix.GetUpperBound(0);
+            int i = 0;
+            int j = matrix.GetUpperBound(1);
+            while (i <= rowCount && j >= 0)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                if (num > matrix[i, j] )
                 {
-                    if (Search2D(matrix, row, col, word))
-                    {
-                        System.Console.WriteLine("Pattern found at " + row + " " + col);
-                    }
+                    i++;
                 }
-            }
-        }
-        public static bool Search2D(char[,] matrix, int row, int col, string word)
-        {
-            int[] x = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
-            int[] y = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
-            if (matrix[row, col] != word[0])
-                return false;
-            int len = word.Length;
-            for (int dir = 0; dir < 8; dir++)
-            {
-                // Initialize starting point for current direction  
-                int k, rd = row + x[dir], cd = col + y[dir];
-                for (k = 1; k < len; k++)
+                else if (num < matrix[i, j])
                 {
-                    if (rd >= matrix.GetLength(0) || rd < 0 || cd >= matrix.GetLength(1) || cd < 0)
-                        break;
-                    if (matrix[rd, cd] != word[k])
-                        break;
-                    rd += x[dir];
-                    cd += y[dir];
+                    j--;
                 }
-                // If all character matched, then value of must
-                // be equal to length of word
-                if (k == len)
+                else
                     return true;
             }
             return false;
