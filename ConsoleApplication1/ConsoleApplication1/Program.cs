4b08d12cc4ec19b96f1535cd4c3c2fad17378964
tnayanam <nayatanuj@gmail.com>
2018-05-31 00:04:59 -0400

    76. Minimum Window Substring

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,39 +1,35 @@
+using System;
+
 namespace MainSolution
 {
     class Program
     {
         public static void Main()
         {
-            int[,] matrix = new int[,]
-                             {
-                                {1,  2,  3},
-                                {4,  5,  6},
-                                {7,  8,  9},
-                                {10, 11, 12},
-                                {13, 14, 15}
-                             };
-            System.Console.WriteLine(Search(matrix,1));
-        }
-
-        public static bool Search(int[,] matrix, int num)
-        {
-            int rowCount = matrix.GetUpperBound(0);
-            int i = 0;
-            int j = matrix.GetUpperBound(1);
-            while (i <= rowCount && j >= 0)
+            int start = Int32.MaxValue;
+            int end = Int32.MinValue;
+            string searchString = "";
+            string S = "qasbvgttdf";
+            for (int i = 0; i < searchString.Length; i++)
             {
-                if (num > matrix[i, j] )
+                for (int j = 0; j < S.Length; j++)
                 {
-                    i++;
+                    if (searchString[i] == S[j])
+                    {
+                        if (j < start)
+                        {
+                            start = j;
+                        }
+                        if (j > end)
+                        {
+                            end = j;
+                        }
+                        break;
+                    }
                 }
-                else if (num < matrix[i, j])
-                {
-                    j--;
-                }
-                else
-                    return true;
             }
-            return false;
+            
+            Console.WriteLine(S.Substring(start, end-start+1));
         }
     }
 }
