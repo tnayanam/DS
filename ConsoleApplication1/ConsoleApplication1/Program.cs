4880dc9f3586babf8b2e2d950adb31f5391ad82b
unknown <tnayanam@syr.edu>
2018-06-12 11:36:03 -0400

    First Non Repeating Character SessionM company

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,12 +1,34 @@
 // 1 1 2 3 5 8 13 21 34 55 89
+using System.Collections.Generic;
+
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //https://www.youtube.com/watch?v=NGtt7GJ1uiM&list=PLJicmE8fK0EiFRt1Hm5a_7SJFaikIFW30&index=16
+            string str = "throttle";
+            System.Console.WriteLine (Non(str));
+        }
 
+        public static char FirstNonRepeatingCharacter(string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.ContainsKey(str[i]))
+                    dict[str[i]] = dict[str[i]] + 1;
+                else
+                    dict[str[i]] = 1;
+
+            }
+
+            foreach (var item in dict)
+            {
+                if (item.Value == 1)
+                    return item.Key;
+            }
+            return ' ';
         }
     }
 }
